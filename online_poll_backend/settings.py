1c2622eccf94244dc22f0bed15b4bbf826097696
Happiness_dah <132366448+Happiness7230@users.noreply.github.com>
2025-11-27 16:23:11 +0000

    Update settings.py

--- online_poll_backend/settings.py
+++ online_poll_backend/settings.py
@@ -227,13 +227,11 @@ REDIS_URL = os.getenv("REDIS_URL", "redis://127.0.0.1:6379/1")
 
 CACHES = {
     "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": REDIS_URL,
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "LOCATION": "polls-cache"
     }
 }
+
 
 
 # Swagger settings
